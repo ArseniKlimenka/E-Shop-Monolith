ba5c15bbbb8bbd75ad25af8aff09687350d58be9
Klimenko Arseni <a.klimenko.vironit@vironit.ru>
2020-07-21 14:53:22 +0300

    Added admin CRUD operations

---
+++ ElectroStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using ElectroStore.Domain.Abstract;
+using ElectroStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectroStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("Изменения к фильму \"{0}\" были сохранены", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Что-то не так со значениями данных
+                return View(product);
+            }
+        }
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+        [HttpPost]
+        public ActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("Фильм \"{0}\" был удалена",
+                    deletedProduct.Name);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
